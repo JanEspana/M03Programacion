589a78486ceef2066374f3c3a2cd3f92f5970087
Jan <147418240+JanEspana@users.noreply.github.com>
2024-01-10 12:33:00 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Binario
+{
+    public class Ex25
+    {
+        public static void Main()
+        {
+            Console.Write("Escribe un número y se devolverá su binario: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Binario(num));
+        }
+        public static int Binario(int num)
+        {
+            if (num == 0)
+            {
+                return 0;
+            }
+            return num%2 + 10 * Binario(num/2);
+        }
+    }
+}
