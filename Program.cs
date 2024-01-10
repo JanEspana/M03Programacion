7e6a7863a5ff3b3bdc16563b5782679c354ccaa6
Jan <147418240+JanEspana@users.noreply.github.com>
2024-01-10 12:19:20 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace SumDigits
+{
+    public class Ex26
+    {
+        public static void Main()
+        {
+            const string MSG = "Introdueix un nombre: ";
+            int input;
+            Console.WriteLine(MSG);
+            input = Convert.ToInt32(Console.ReadLine());
+            Console.Write(SumDigitsRec(input));
+        }
+        public static int SumDigitsRec(int num)
+        {
+            if (num == 0)
+            {
+                return 0;
+            }
+            return num % 10 + SumDigitsRec(num / 10);
+        }
+    }
+}
